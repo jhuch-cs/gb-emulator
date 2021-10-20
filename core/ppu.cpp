6ca6068434e6a72308ef0dbf304db337f244a527
Ian Kirkham <imkirk56@gmail.com>
2021-10-20 17:55:56 -0600

    Initial naive implementation of PPU

--- core/ppu.cpp
+++ core/ppu.cpp
@@ -1,1 +1,109 @@
 #pragma once
+
+#include "./util.hpp"
+#include "./ppu.hpp"
+
+// PPU::PPU(CPU& cpu, MMU& mmu) {
+//   PPU::cpu = cpu;
+//   PPU::mmu = mmu;
+// }
+
+u8* PPU::getFrameBuffer() {
+  return frameBuffer;
+}
+
+void PPU::step(u8 cpuCyclesElapsed) {
+
+  // get updated lcd control
+  u8 lcdCtrl = mmu.read(lcdc);
+  // get updated lcd status
+  u8 lcdStat = mmu.read(stat);
+  // get ly (current scanline)
+  u8 scanline = mmu.read(ly);
+
+  //...
+
+  // for the amount of cycles:
+  // switch based on current mode
+
+  // check current scanline == 144, then enter VBLANK
+  // reset scanline to 0 if > 153 (end of VBLANK)
+  // when less than 144, call drawScanLine(lcd_ctrl)
+}
+
+// PPU mode typically goes from OAM -> VRAM -> HBLANK, repeating until VBLANK (aka mode 1)
+// 456 clocks (114 cycles) to draw one scanline
+  // OAM - 80 clocks (20 cycles)
+  // VRAM - 172-289 clocks (43-72 cycles) [set default to start at 172?]
+  // HBLANK - 87-204 clocks (22-51) cycles) (depending on prev) [set default to start at 289?]
+void PPU::drawScanLine(u8 lcdCtrl) {
+
+  // check if lcd is enabled (or is this done in step()?)
+
+  if (checkBit(lcdCtrl, 0)) {
+    renderTiles(lcdCtrl);
+  }
+  if (checkBit(lcdCtrl, 1)) {
+    renderSprites();
+  }
+}
+
+// Tile Data in one of two locations: (controled by LCDC Bit 4)
+  // 0X800-0X8FFF (unsigned numbers from 0 - 255)
+  // 0X8800-0X97FF (singed nubmers from -128 - 127)
+// Tile Map in 0x9800-0x9BFF or 0x9C00-0X9FFF
+  // Window tile map area	determined by Bit 6 in LCDC -> 0=9800-9BFF, 1=9C00-9FFF
+  // Backgrond tile map area determined by Bit 3 in LCDC -> 0=9800-9BFF, 1=9C00-9FFF
+void PPU::renderTiles(u8 lcdCtrl) {
+
+  u8 scrollY = mmu.read(scy);
+  u8 scrollX = mmu.read(scx);
+  u8 windowY = mmu.read(wy);
+  u8 windowX = mmu.read(wx) - 7;
+
+  //lcdCtrl
+  // Bit 6	Window tile map area	0=9800-9BFF, 1=9C00-9FFF
+  // Bit 5	Window enable	0=Off, 1=On
+  // Bit 4	BG and Window tile data area	0=8800-97FF, 1=8000-8FFF
+  // Bit 3	BG tile map area	0=9800-9BFF, 1=9C00-9FFF
+
+}
+
+// OAM table: 0xFE00 - 0XFE9F
+// OAM entries:
+  // Byte 0 - Y pos + 16
+  // Byte 1 - X pos + 8
+  // Byte 2 - Tile Index
+    // 8x8 mode (LCDC bit 2 = 0), unsigned val 0x00 - 0xFF
+    // 8x16 mode (LCDC bit 2 = 1), every two tiles form a sprite (top then bottom)
+  // Byte 3 - Attributes/Flags
+    // Bit 7   BG and Window over OBJ (0=No, 1=BG and Window colors 1-3 over the OBJ)
+    // Bit 6   Y flip          (0=Normal, 1=Vertically mirrored)
+    // Bit 5   X flip          (0=Normal, 1=Horizontally mirrored)
+    // Bit 4   Palette number  **Non CGB Mode Only** (0=OBP0, 1=OBP1)
+    // Bit 3   Tile VRAM-Bank  **CGB Mode Only**     (0=Bank 0, 1=Bank 1)
+    // Bit 2-0 Palette number  **CGB Mode Only**     (OBP0-7)
+void PPU::renderSprites() {
+
+  // check if sprites are 8x8 or 8x16
+  u8 lcdCtrl = mmu.read(lcdc);
+  bool use8x16 = checkBit(lcdCtrl, 2);
+
+  // can render up to 40 sprites, iterate through OAM table
+  for (int index = 0; index < 40; index++) {
+    
+  }
+}
+
+// During mode VRAM: OAM, HBLANK, and VBLANK can accessible
+// During mode OAM: HBLANK and VBLANK accessible
+  // During restricted modes, any attempt to read returns $FF, any attempt to write are ignored
+// PPU::cpuReadVRAM()
+// PPU::cpuWriteVRAM()
+// PPU::cpuReadOAM()
+// PPU::cpuWriteOAM()
+
+// Helper Functions
+bool checkBit(u8 value, u8 index) { 
+  return (value & (1 << index)) != 0;
+}
--- core/ppu.hpp
+++ core/ppu.hpp
@@ -22,17 +22,92 @@ public:
   // have to update the buffer when SDL asks for it
   u8* getFrameBuffer();
 
-  // probably a lot of other public interface stuff that you'll discover in implementation
-  // See https://gbdev.io/pandocs/Rendering.html
+  // TODO: decide whether this logic should live in the PPU or if we should have the MMU handle 
+  //       these by allowing it to access the current mode of the PPU
+  // cpuReadVRAM()
+  // cpuWriteVRAM()
+  // cpuReadOAM()
+  // cpuWriteOAM()  
     
 private:
   CPU cpu;
   MMU mmu;
   Mode mode;
 
+  // LCDC (0xFF40) - LCD Control
+    // Bit 7	LCD and PPU enable	0=Off, 1=On
+    // Bit 6	Window tile map area	0=9800-9BFF, 1=9C00-9FFF
+    // Bit 5	Window enable	0=Off, 1=On
+    // Bit 4	BG and Window tile data area	0=8800-97FF, 1=8000-8FFF
+    // Bit 3	BG tile map area	0=9800-9BFF, 1=9C00-9FFF
+    // Bit 2	OBJ size	0=8x8, 1=8x16
+    // Bit 1	OBJ enable	0=Off, 1=On
+    // Bit 0	BG and Window enable/priority	0=Off, 1=On
+  const u8 lcdc = 0xFF40;
+
+  // STAT (0xFF41) - LCD Status
+    // Bit 6 - LYC=LY STAT Interrupt source         (1=Enable) (Read/Write)
+    // Bit 5 - Mode 2 OAM STAT Interrupt source     (1=Enable) (Read/Write)
+    // Bit 4 - Mode 1 VBlank STAT Interrupt source  (1=Enable) (Read/Write)
+    // Bit 3 - Mode 0 HBlank STAT Interrupt source  (1=Enable) (Read/Write)
+    // Bit 2 - LYC=LY Flag                          (0=Different, 1=Equal) (Read Only)
+    // Bit 1-0 - Mode Flag                          (Mode 0-3, see below) (Read Only)
+    //           0: HBlank
+    //           1: VBlank
+    //           2: Searching OAM
+    //           3: Transferring Data to LCD Controller
+  const u8 stat = 0xFF41;
+
+  // ScrollY (0xFF42) - y pos of background
+  // ScrollX (0xFF43) - x pos of background
+  const u8 scy = 0xFF42;
+  const u8 scx = 0xFF43;
+
+  // LY (0xFF44) - LCD Y Coordinate (aka current scanline)
+  // Holds values 0-153, with 144-153 indicating VBLANK
+  const u8 ly = 0xFF44;
+
+  // LYC (0xFF45) - LY Compare
+  const u8 lyc = 0xFF45;
+
+  // DMA (0xFF46) - DMA Transfer and Start
+  // 160 cycles
+  const u8 dma = 0xFF46;
+
+  // WHITE = 0  #9bbc0f RGB: 155, 188, 15
+  // LIGHT_GRAY = 1 #8bac0f RGB: 139, 172, 15
+  // DARK_GRAY = 2  #306230 RGB: 48, 98, 48
+  // BLACK = 3  #0f380f RGB: 15, 56, 15
+
+  // bgp (0xFF47) - BG palette data
+    // Bit 7-6 Color for index 3
+    // Bit 5-4 Color for index 2
+    // Bit 3-2 Color for index 1
+    // Bit 1-0 Color for index 0
+  const u8 bgp = 0xFF47;
+
+  // obp0 (0xFF48) - OBJ palette 0 data
+  // Just like bgp but bits 1-0 ignored because color index 0 is transparent for sprites
+  const u8 obp0 = 0xFF48;
+
+  // obp1 (0XFF49) - OBJ palette 1 data
+  const u8 obp1 = 0xFF49;
+
+  // WindowY (0xFF4A) - y pos of window
+  // WindowX (0xFF4B) - x pos - 7 of window
+  const u8 wy = 0xFF4A;
+  const u8 wx = 0xFF4B;
+
+  void drawScanLine(u8 lcdCtrl);
+  void renderTiles(u8 lcdCtrl);
+  void renderSprites();
+
   // 160 x 144 x 3 (last dimenstion is pixel, rgb)
   static u8* frameBuffer;
 
   // 256 x 256
   u8* backgroundMap;
+
+  // Helper Functions
+  bool checkBit(u8 value, u8 index);
 };
