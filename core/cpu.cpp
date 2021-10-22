9c3f86314fd19766ba6ed63609ffc727ba34a3fd
gagemeservy <gage.meservy@gmail.com>
2021-10-22 10:17:10 -0600

    Fleshed out functions
    Put more code in the functions I was working on. Set registers to place holder values.

--- core/cpu.cpp
+++ core/cpu.cpp
@@ -3,8 +3,6 @@
 #include "./util.hpp"
 #include "cpu.hpp"
 
-//set all register locations and Program counter variables up here I think
-
 //flags in AF register
 const u8 zero_flag_z = 0x80; //it's the 7th bit
 const u8 subtraction_flag_n = 0x40; //it's the 6th bit
@@ -18,6 +16,15 @@ CPU::CPU(MMU& mmu, u16* bootRomLocation){
     
     //setting the pc to start where the boot rom is located
     this->pc = *bootRomLocation;
+
+    //set all register locations and Program counter variables here
+    //does it matter what their starting values are?
+    /********UPDATE THESE. THESE ARE PLACE HOLDER VALUES********/
+    this->af=0x0000;
+    this->bc=0x0000;
+    this->de=0x0000;
+    this->hl=0x0000;
+    this->sp=0x0000;
 }
 
 u8 CPU::step(){
@@ -39,71 +46,125 @@ u8 CPU::exec(){
         switch(opCode){
             case 0x86:
                 //ADD A,(HL)
-                u8 address_hl = mmu.read(hl);
-                /***********a += mmu.read(address_hl);**********/
+                u8 value_at_hl = mmu.read(hl);
+                u8 a = getHighByte(af);
+                a += value_at_hl;
+                setHighByte(&af, a);
                 break;
 
             case 0xCD:
                 //CALL a16
                 sp--;
                 sp--;
-                u16 address_sp = mmu.read16Bit(sp);
-                mmu.write(address_sp, pc);
+                mmu.write(sp, pc);
 
-                u16 nn = mmu.read16Bit(pc++);
-                pc = nn;
+                
+                //PC NEEDS TO BE INCREMENTED TWICE ON 16 BIT READ
+                u16 nn_nn = mmu.read16Bit(pc++);
+                pc++;
+                pc = nn_nn;
                 break;
 
             case 0xFE:
                 //CP d8
                 //CP is a subtraction from A that doesn't update A, only the flags it would have set/reset if it really was subtracted.
                 u8 n = mmu.read(pc++);
-                /*********u8 compare = a - n;************/
+                u8 compare = a - n;
+
                 //set subtraction flag
-                /********if (compare < 0){
+                u8 f = getLowByte(af);
+                f = f | subtraction_flag_n;
+                setHighByte(&af, f);
+                
+                if (compare < 0){
                     //set carry flag
+                    u8 f = getHighByte(af);
+                    f = f | carry_flag_c;
+                    setHighByte(&af, f);
                 }
                 else if(compare == 0){
                     //set zero flag
-                }*********/
+                    u8 f = getHighByte(af);
+                    f = f | zero_flag_z;
+                    setHighByte(&af, f);
+                }
                 //need to look up when to set half cary flag
                 break;
             case 0xBE:
                 //CP (HL)
                 //CP is a subtraction from A that doesn't update A, only the flags it would have set/reset if it really was subtracted.
                 u8 valueAtHL = mmu.read(hl);
-                /**********u8 compare = a - valueAtHL;***********/
-                //how/what flags do we set/reset here
+                u8 compare = a - valueAtHL;
+
+                //set subtraction flag
+                u8 f = getLowByte(af);
+                f = f | subtraction_flag_n;
+                setHighByte(&af, f);
+                
+                if (compare < 0){
+                    //set carry flag
+                    u8 f = getHighByte(af);
+                    f = f | carry_flag_c;
+                    setHighByte(&af, f);
+                }
+                else if(compare == 0){
+                    //set zero flag
+                    u8 f = getHighByte(af);
+                    f = f | zero_flag_z;
+                    setHighByte(&af, f);
+                }
+                //need to look up when to set half cary flag
                 break;
 
             case 0x3D:
                 //DEC A
                 //a--;
+                u8 a = getHighByte(af);
+                a--;
+                setHighByte(&af, a);
                 break;
             case 0x05:
                 //DEC B
                 //b--;
+                u8 b = getHighByte(bc);
+                b--;
+                setHighByte(&bc, b);
                 break;
             case 0x0D:
                 //DEC C
                 //c--;
+                u8 c = getLowByte(bc);
+                c--;
+                setLowByte(&bc, c);
                 break;
             case 0x15:
                 //DEC D
                 //d--;
+                u8 d = getHighByte(de);
+                d--;
+                setHighByte(&de, d);
                 break;
             case 0x1D:
                 //DEC E
                 //e--;
+                u8 e = getLowByte(de);
+                e--;
+                setLowByte(&de, e);
                 break;
 
             case 0x04:
                 //INC B
                 //b++;
+                u8 b = getHighByte(bc);
+                b++;
+                setHighByte(&bc, b);
                 break;
             case 0x0C:
                 //INC C
                 //c++;
+                u8 c = getLowByte(bc);
+                c++;
+                setLowByte(&bc, c);
                 break;
             case 0x13:
                 //INC DE
@@ -112,6 +173,9 @@ u8 CPU::exec(){
             case 0x24:
                 //INC H
                 //h++;
+                u8 h = getHighByte(hl);
+                h++;
+                setHighByte(&hl, h);
                 break;
             case 0x23:
                 //INC HL
@@ -120,18 +184,26 @@ u8 CPU::exec(){
             
             case 0x20:
                 //JR NZ,r8
-                /********if ((subtraction_flag_n & f) && (zero_flag_z & f)){
-                    //execute jump
-                }*********/
+                u8 f = getLowByte(af);
+                if ((subtraction_flag_n & f) && (zero_flag_z & f)){
+                    //execute relative jump, nn is signed
+                    s8 nn = mmu.read(pc++);
+                    pc += nn;
+                }
                 break;
             case 0x28:
                 //JR Z,r8
-                /*****if (zero_flag_z & f){
-                    //execute jump
-                }******/
+                u8 f = getLowByte(af);
+                if (zero_flag_z & f){
+                    //execute relative jump, nn is signed
+                    s8 nn = mmu.read(pc++);
+                    pc += nn;
+                }
                 break;
             case 0x18:
-                //JR r8
+                //JR r8, nn is signed
+                s8 nn = mmu.read(pc++);
+                pc += nn;
                 break;
             
             case 0x3E:
@@ -266,6 +338,7 @@ u8 CPU::setLowByte(u16* destination, u8 value){
 }
 
 // Map from the register code to the register value itself
+//honestly not sure what these are for
 u8 CPU::get8BitRegister(u8 registerValue){
 
 }
